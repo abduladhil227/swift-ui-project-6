74ceb3ac4f9d3a4f8578322a9c7c059a0ade98ca
abduladhil227 <abduladhil006@gmail.com>
2024-08-26 23:34:10 +0530

    customizing animation in swift

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    @State private var secondAnimationAmount = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Me!"){
+            animationAmount += 1
+            
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+//        .blur(radius: (animationAmount - 1) * 3)
+        //implicit animation
+//        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+        .animation(
+            .easeInOut(duration: 2)
+//            .delay(1),
+                .repeatCount(3, autoreverses: true),
+            value: animationAmount)
+        
+        Button("Tap Me!"){
+//            animationAmount += 1
+            
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(secondAnimationAmount)
+                .opacity(2 - secondAnimationAmount)
+                .animation(.easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false), value: secondAnimationAmount)
+            
+        )
+        .onAppear{
+            secondAnimationAmount = 2
+        }
     }
 }
 
