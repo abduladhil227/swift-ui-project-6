5a75b11a7de270dd75fbd41f39e3d1d2f7839e9d
abduladhil227 <abduladhil006@gmail.com>
2024-08-26 23:47:25 +0530

    animation binding in swift

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        //binding animation when we increase the value using stepper it will animate
+        //but it won't animate when we press the button because the button value is not binded to animation
+        Stepper("Select amount", value: $animationAmount.animation(
+            .easeInOut(duration: 1)
+            .repeatCount(3, autoreverses: true)
+        ), in: 1...10)
+        
+        Spacer()
+        
+        Button("Tap Me"){
+            animationAmount  += 1
         }
-        .padding()
+        .padding(40)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
     }
 }
 
